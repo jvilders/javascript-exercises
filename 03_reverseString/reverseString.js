706e7949c6b736d59d339e9b49288dd62f869ad9
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-02-04 13:35:28 +0100

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    const charArray = [];
 
+    for(let i = stringToReverse.length - 1; i >= 0; i--){
+        charArray.push(stringToReverse.charAt(i));
+    };
+
+    return charArray.join('');
 };
 
 // Do not edit below this line
