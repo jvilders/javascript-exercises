69f3886134d4f310b34fd27dffabea9edac8e6c5
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-02-04 13:47:28 +0100

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    const removeSet = new Set(remove)
+    return arr.filter(el => !removeSet.has(el))
 };
 
 // Do not edit below this line
