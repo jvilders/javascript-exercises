9eb1fb1c4412a02e85707af28caf937c8fc08378
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-02-04 14:44:21 +0100

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (possiblePalindrome) {
+    if(typeof possiblePalindrome !== 'string') return 'ERROR'
 
+    // remove spaces, punctuation, make lowercase
+    const normalizedString = possiblePalindrome.replaceAll(/[!., ]/g, '').toLowerCase()
+
+    for(let p1 = 0, p2 = normalizedString.length - 1; p1 < p2; p1++, p2--){
+        if(normalizedString.charAt(p1) != normalizedString.charAt(p2)) return false
+    }
+    return true
 };
 
 // Do not edit below this line
