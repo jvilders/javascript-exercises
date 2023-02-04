04f12257c98aab9dec748a6f9caa3069644d735e
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-02-04 14:18:29 +0100

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  return ((tempFahrenheit - 32) * (5 / 9)).toFixed(1) / 1
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  return (tempCelsius * (9 / 5) + 32).toFixed(1) / 1
 };
 
 // Do not edit below this line
