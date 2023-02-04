7859dc16ae2559a6fe0552d24b6d3c8588410755
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-02-04 14:00:35 +0100

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    for(n of [n1, n2]){
+        if(typeof n !== 'number' || n < 0) return 'ERROR';
+    };
+    if(n1 === n2) return n1;
 
+    const [begin, end] = [n1, n2].sort();
+    return ((end + 1) * end) / 2 - (begin * (begin - 1)) / 2;
 };
 
 // Do not edit below this line
