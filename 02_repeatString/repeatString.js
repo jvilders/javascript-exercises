734090e5dd76f838476743e8f66e1e3779c32825
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-02-04 13:28:09 +0100

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, repetitions) {
+    if(repetitions < 0){
+        return 'ERROR'
+    }
 
+    let result = '';
+    for(;repetitions > 0; repetitions--){
+        result += stringToRepeat
+    }
+    return result
 };
 
 // Do not edit below this line
